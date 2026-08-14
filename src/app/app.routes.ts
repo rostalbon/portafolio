b4860f79cc4545f139c32dc2d1a0c23f4997ff17
Rocío <rostalbon@gmail.com>
2026-08-14 08:07:01 -0300

    Se creó el componente projects/create-project y se configuró su ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,14 @@ import { Projects } from './views/projects/projects';
 import { Contact } from './views/contact/contact';
 import { Home } from './views/home/home';
 import { SignIn } from './views/sign-in/sign-in';
-import { CreateProject } from './views/projects/create-project/create-project';
+import { CreateProject } from './views/create-project/create-project';
 
 export const routes: Routes = [
     {path:"", redirectTo:"home", pathMatch:'full'},
     {path:"about", component:About},
-    {path:"projects", component:Projects, children: [
-        {path: "create-project", component:CreateProject}
-    ]},
+    {path:"projects", component:Projects},
     {path:"contact", component:Contact},
     {path:"home", component:Home},
     {path: "sign-in", component:SignIn},
+    {path: "create-project", component:CreateProject}
 ];
