97f1f09e35619b0e2707b86e217666b6791eedcd
Rocío <rostalbon@gmail.com>
2026-08-14 09:54:22 -0300

    Se creó el formulario reactivo en el ty

--- src/app/views/upload-project/upload-project.ts
+++ src/app/views/upload-project/upload-project.ts
@@ -1,9 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-upload-project',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './upload-project.html',
   styleUrl: './upload-project.css',
 })
-export class UploadProject {}
+export class UploadProject {
+  private formBuilder = inject(FormBuilder)
+  uploadProjectForm = this.formBuilder.group({
+    title: ['', [Validators.required]],
+    html: [''],
+    css: [''],
+    javascript: [''],
+    typescript: [''],
+    python: [''],
+    others: [''],
+    year: ['', [Validators.required, Validators.min(1950), Validators.max(2026)]],
+    url: ['', [Validators.pattern(/^(https?:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9_-]+\/?.*$/)]],
+    description: [''],
+    main: ['']
+  })
+  get Title() {
+    return this.uploadProjectForm.get("title")
+  }
+  get Html() {
+    return this.uploadProjectForm.get("html")
+  }
+  get Css() {
+    return this.uploadProjectForm.get("css")
+  }
+  get Javascript() {
+    return this.uploadProjectForm.get("javascript")
+  }
+  get Typescript() {
+    return this.uploadProjectForm.get("typescript")
+  }
+  get Python() {
+    return this.uploadProjectForm.get("python")
+  }
+  get Others() {
+    return this.uploadProjectForm.get("others")
+  }
+  get Year() {
+    return this.uploadProjectForm.get("year")
+  }
+  get Url() {
+    return this.uploadProjectForm.get("url")
+  }
+  get Description() {
+    return this.uploadProjectForm.get("description")
+  }
+  get Main() {
+    return this.uploadProjectForm.get("main")
+  }
+}
