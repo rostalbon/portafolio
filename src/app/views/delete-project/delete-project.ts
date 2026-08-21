5239bfae90618481946ceb34e7872d9915b51360
Rocío <rostalbon@gmail.com>
2026-08-21 15:39:38 -0300

    Se completó la clase DeleteProject

--- src/app/views/delete-project/delete-project.ts
+++ src/app/views/delete-project/delete-project.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ProjectsService } from '../../services/projects.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-delete-project',
-  imports: [RouterLink],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './delete-project.html',
   styleUrl: './delete-project.css',
 })
-export class DeleteProject {}
+export class DeleteProject {
+  projects: any = []
+  private formBuilder = inject(FormBuilder)
+  constructor(private service: ProjectsService, private cdr: ChangeDetectorRef) {
+    this.service.getProjects().subscribe({
+      next: (data) => {
+        this.projects = data
+        console.log(this.projects)
+      },
+      error: (error) => console.error(error),
+      complete: () => {
+        this.cdr.detectChanges()
+      }
+    })
+  }
+  deleteProjectForm = this.formBuilder.group({
+    select: []
+  })
+  get Select() {
+    return this.deleteProjectForm.get("select")
+  }
+  formValues = this.deleteProjectForm.value
+  deleteProject() {
+    console.log(this.formValues.select)
+  }
+}
