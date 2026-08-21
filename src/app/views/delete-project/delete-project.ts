2598366fea56b55b6fdc843396af6302514dd3e8
Rocío <rostalbon@gmail.com>
2026-08-21 14:06:18 -0300

    Se importó RouterLink a delete-project

--- src/app/views/delete-project/delete-project.ts
+++ src/app/views/delete-project/delete-project.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-delete-project',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './delete-project.html',
   styleUrl: './delete-project.css',
 })
