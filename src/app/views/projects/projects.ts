5d9062acf61b65ff21c18660724bb4cef44074a1
Rocío <rostalbon@gmail.com>
2026-08-20 22:43:18 -0300

    Se añadió un botón para crear proyecto en projects.html

--- src/app/views/projects/projects.ts
+++ src/app/views/projects/projects.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { ProjectsService } from '../../services/projects.service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
