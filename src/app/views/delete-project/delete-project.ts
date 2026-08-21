670ce08a0392256f3c5b7cd3f04cba66fe3071b7
Rocío <rostalbon@gmail.com>
2026-08-21 14:03:00 -0300

    Se añadió el componente delete-project

---
+++ src/app/views/delete-project/delete-project.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-project',
+  imports: [],
+  templateUrl: './delete-project.html',
+  styleUrl: './delete-project.css',
+})
+export class DeleteProject {}
